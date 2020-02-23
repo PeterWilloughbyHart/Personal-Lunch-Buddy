3540edebcce19607959559e14ac0e5f16c6f1b49
Peter <oliverdog4@gmail.com>
2020-02-22 20:30:31 -0500

    Updates

---
+++ precache-manifest.0ab5e786fec0e7d626e1b6ea06976b0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e04841d3bd0d45eaa0a89b5286c0fb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26b3e6b7d494ce151bf9",
+    "url": "/static/css/main.40798b2c.chunk.css"
+  },
+  {
+    "revision": "9bb72b6690de114f3caf",
+    "url": "/static/js/2.4dcfb8a9.chunk.js"
+  },
+  {
+    "revision": "26b3e6b7d494ce151bf9",
+    "url": "/static/js/main.12ee81d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
